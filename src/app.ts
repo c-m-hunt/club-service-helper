d2331725375060267c30e3776b1806308c4db890
Chris Hunt <chris.hunt@hudl.com>
2019-11-08 09:55:28 +0000

    Add basic Slack integration

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-import Twitter from './lib/social/twitter';
-import Contentful from './lib/content/contentful';
-import config from './config';
-
+import config from "./config";
+import Contentful from "./lib/content/contentful";
+import Facebook from "./lib/social/facebook";
+import Slack from "./lib/social/slack";
+import Twitter from "./lib/social/twitter";
 
 const t = new Twitter(config.clientConfig.twitter);
-const c = new Contentful(config.clientConfig.contentful)
+const c = new Contentful(config.clientConfig.contentful);
+const f = new Facebook(config.clientConfig.facebook);
 
-const message = `Message to update`;
+const message = `Reminder: Club EGM at The Pavilion - 7:30pm on Monday, 18th November. All members should have received details by email. Any problems - DM us.`;
 
 // FB.api(
 //   `/${pageId}/feed`,
@@ -18,11 +20,12 @@ const message = `Message to update`;
 // )
 
 // t.updateStatus(message);
-
-// 
-// 
+// f.postToWall(message);
 
 // c.get100ClubResults()
 //   .then(results => {
 //     console.log(results);
 //   });
+
+const s = new Slack(config.clientConfig.slack);
+// s.postMessage(message);
